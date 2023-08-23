7affba5456ab8f8f4c71bc28e7c59b8a588da9d5
1·231 <123213>
2023-08-23 16:04:04 +0800

    11

---
+++ 后台/src/test/java/com/ledger/es_test1/mapper/ExcelMapperTest.java
@@ -0,0 +1,26 @@
+package com.ledger.es_test1.mapper;
+
+import com.ledger.es_test1.domain.EnglishWords;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+
+
+@SpringBootTest
+class ExcelMapperTest {
+    @Resource
+    private ExcelMapper excelMapper;
+
+    @Test
+    void testDao() {
+//        EnglishWords englishWord = new EnglishWords(1,"高频",1,"1","work","[wɜːrk]","vi.工作;产生...作用;争取 v.(使) 运转 n.工作;工作成果;作品");
+//        EnglishWords englishWord1 = new EnglishWords(2,"高频",1,"1","coworker","[wɜːrk]","n.同事");
+//
+//        excelMapper.insert(englishWord);
+//        excelMapper.insert(englishWord1);
+    }
+
+
+}
---
+++ 后台/src/test/java/com/ledger/es_test1/service/ExcelServiceTest.java
@@ -0,0 +1,49 @@
+package com.ledger.es_test1.service;
+
+import com.ledger.es_test1.domain.EnglishWords;
+import com.ledger.es_test1.response.Result;
+import com.ledger.es_test1.vo.TableVo;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Slf4j
+class ExcelServiceTest {
+    @Resource
+    private ExcelService excelService;
+
+    @Test
+    void analyze() {
+        excelService.remove(null);
+    }
+
+    @Test
+    void testMethod2() {
+        ArrayList<String> list = new ArrayList<>();
+//        list.add("所有单词");
+        list.add("高频");
+        list.add("中频");
+        Result<TableVo>  all = excelService.getDataBySize2(10, list);
+
+        TableVo data = all.getData();
+        List<EnglishWords> tableBody = data.getTableBody();
+
+        for (EnglishWords datum : tableBody) {
+            log.info(datum.toString());
+        }
+    }
+
+    @Test
+    void get() {
+        List<EnglishWords> list = excelService.list();
+        list.forEach(System.out::println);
+    }
+}
